2f4e849056e272f016b2e9f0ad3de000768b8228
Byunghoon Hwang <fuzzycom@gmail.com>
2016-09-27 13:28:34 +0900

    Report card: Show the sum of each grade
    Signed-off-by: Byunghoon Hwang <fuzzycom@gmail.com>

--- report_card.c
+++ report_card.c
@@ -1,9 +1,34 @@
-packing knapsack: Change basic code
+/*
+ * Packing knapsack question
+ *
+ * Copyright (C) 2016, Taeung Song <treeze.taeung@gmail.com>
+ *
+ */
+#include <stdio.h>
 
-Previous basic code hadn't data structures.
-And functions they contain were ambiguous
-so change this infrastructure code following normal way
-already known.
+struct knapsack {
+	unsigned int limited_wgt;
+	unsigned int max_price;
+};
 
+struct jewelry {
+	unsigned int wgt;
+	unsigned int price;
+};
 
-asdfasdfasdf
+void pack_knapsack(struct knapsack *knapsack_list,
+		      struct jewelry **usable_jewels)
+{
+	/* Case by case pack knapsack following maximum
+	 * price per limited weight.
+	 */
+}
+
+int get_maxprice(struct knapsack *knapsack_list, struct jewelry *jewelry)
+{
+	/* Get maximum price based on limited weight */
+}
+
+int main(int argc, const char **argv)
+{
+}
